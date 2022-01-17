336ac39653664d00a6a778f49f2ca534698d1529
NoahAncolie <noahancolie@gmail.com>
2022-01-17 17:21:59 +0100

    AD

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+/**option 2 */
+
+// $('.navbar a').on('click', function(e) {
+//     if(this.hash !== '') {
+//         e.preventDefault();
+
+//         const hash = this.hash;
+
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 800);
+//     }
+// })
+
+/**Option 3 */
+
+// const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+//     speed: 800
+// });
+
+
+
+
+
+
+
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
