3fc7629bd5286dc5e7ec12f6b7aa53d04bc06a34
Adrien Benaceur <74266541+NoahAncolie@users.noreply.github.com>
2022-06-06 18:40:01 +0200

    Delete src directory

--- src/views/CvView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <h1>Différents types de textes</h1>
-  <ul>
-    <li>Normal</li>
-    <li class="text-medium">Medium</li>
-    <li class="text-muted">Muted</li>
-  </ul>
-</template>
--- src/views/Test.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <p>This is the test</p>
-</template>
